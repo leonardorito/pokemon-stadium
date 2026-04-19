7ab5be76dcb1c596658f0cc07758fb031aaff69a
leonardorito <leo07291995@gmail.com>
2026-04-19 20:36:47 -0300

    feat(mobile): add backend URL constants

---
+++ mobile/constants/config.ts
@@ -0,0 +1,6 @@
+export const DEFAULT_BACKEND_URL =
+  "https://pokemon-stadium-backend-production-0030.up.railway.app";
+
+export const LOCALHOST_BACKEND_URL = "http://localhost:8080";
+
+export const BACKEND_URL_STORAGE_KEY = "pokemon-stadium:backendUrl";
